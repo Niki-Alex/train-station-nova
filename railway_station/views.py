7de0427ae416f2e8971c36471b2236c09cbaa6e2
Alex_Freud <NikiforenkoAS@i.ua>
2023-09-01 12:15:23 +0300

    Added func filtring by views

--- railway_station/views.py
+++ railway_station/views.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+from django.db.models import Q
 from rest_framework import viewsets
 
 from railway_station.models import (
@@ -27,14 +30,40 @@ from railway_station.serializers import (
 )
 
 
+def _params_to_ints(qs):
+    return [int(str_id) for str_id in qs.split(",")]
+
+
 class StationViewSet(viewsets.ModelViewSet):
     queryset = Station.objects.all()
     serializer_class = StationSerializer
+
+    def get_queryset(self):
+        queryset = self.queryset
+        name = self.request.query_params.get("name")
+
+        if name:
+            queryset = queryset.filter(name__icontains=name)
+
+        return queryset.distinct()
 
 
 class RouteViewSet(viewsets.ModelViewSet):
     queryset = Route.objects.all().select_related("source", "destination")
     serializer_class = RouteSerializer
+
+    def get_queryset(self):
+        queryset = self.queryset
+        source = self.request.query_params.get("source")
+        destination = self.request.query_params.get("destination")
+
+        if source:
+            queryset = queryset.filter(source__name__icontains=source)
+
+        if destination:
+            queryset = queryset.filter(destination__name__icontains=destination)
+
+        return queryset.distinct()
 
     def get_serializer_class(self):
         if self.action == "list":
@@ -50,10 +79,28 @@ class TrainTypeViewSet(viewsets.ModelViewSet):
     queryset = TrainType.objects.all()
     serializer_class = TrainTypeSerializer
 
+    def get_queryset(self):
+        queryset = self.queryset
+        name = self.request.query_params.get("name")
+
+        if name:
+            queryset = queryset.filter(name__icontains=name)
+
+        return queryset.distinct()
+
 
 class TrainViewSet(viewsets.ModelViewSet):
     queryset = Train.objects.all().select_related("train_type")
     serializer_class = TrainSerializer
+
+    def get_queryset(self):
+        queryset = self.queryset
+        train_type = self.request.query_params.get("train_type")
+
+        if train_type:
+            queryset = queryset.filter(train_type__name__icontains=train_type)
+
+        return queryset.distinct()
 
     def get_serializer_class(self):
         if self.action in ["list", "retrieve"]:
@@ -66,6 +113,19 @@ class CrewViewSet(viewsets.ModelViewSet):
     queryset = Crew.objects.all()
     serializer_class = CrewSerializer
 
+    def get_queryset(self):
+        queryset = self.queryset
+        first_name = self.request.query_params.get("first_name")
+        last_name = self.request.query_params.get("last_name")
+
+        if first_name:
+            queryset = queryset.filter(first_name__icontains=first_name)
+
+        if last_name:
+            queryset = queryset.filter(last_name__icontains=last_name)
+
+        return queryset.distinct()
+
 
 class TripViewSet(viewsets.ModelViewSet):
     queryset = Trip.objects.all()
@@ -73,10 +133,24 @@ class TripViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         queryset = self.queryset
+        route = self.request.query_params.get("route")
+        departure_time = self.request.query_params.get("departure_time")
+
+        if route:
+            queryset = (
+                queryset
+                .filter(Q(route__source__name__icontains=route)
+                        | Q(route__destination__name__icontains=route))
+            )
+
+        if departure_time:
+            date = datetime.strptime(departure_time, "%Y-%m-%d").date()
+            queryset = queryset.filter(departure_time__date=date)
+
         if self.action in ["list", "retrieve"]:
             queryset.select_related("route", "train").prefetch_related("crew")
 
-        return queryset
+        return queryset.distinct()
 
     def get_serializer_class(self):
         if self.action == "list":
