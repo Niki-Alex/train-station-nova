6b6a588c409bc26aab03280390807e40bb4f8bb9
Alex_Freud <NikiforenkoAS@i.ua>
2023-09-01 17:07:29 +0300

    Added UserSerializer

--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
+from user.views import CreateUserView
+
 
 urlpatterns = [
-
+    path("register/", CreateUserView.as_view(), name="create"),
 ]
 
 app_name = "user"
